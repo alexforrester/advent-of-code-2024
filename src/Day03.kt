3231e5b056d191762da175d8f6b30ea6d1ed676f
Alex Forrester <aforresterwork@gmail.com>
2024-12-05 10:59:00 +0000

    Update Day03.kt

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,36 +1,72 @@
+sealed class MagicList(val startRange: Int) {
+    data class DO(val start: Int) : MagicList(start)
+    data class DONT(val start: Int) : MagicList(start)
+    data class MATCH(val start: Int, val calc: String) : MagicList(start)
+}
+
 fun main() {
 
-    //val input = readInput("Day02_test")
     val input = readInput("Day03")
+    val multiplyStrings = mutableListOf<String>()
+    val regex = "mul\\(\\d+,\\d+\\)".toRegex()
+    val dontMatchRegex = "don\\'t\\(\\)".toRegex()
+    val doMatchRegex = "do\\(\\)".toRegex()
 
-    val locationSize = input.size
+    var part2Total = 0
 
-    var part1Total = 0
+    val magicList = mutableListOf<MagicList>()
 
-    for (line in 0 until locationSize) {
+    for (line in input) {
 
-        val lineToParse = input[line].also { println(it) }
+        val lineToParse = line
 
-        val regex = "mul\\(\\d+,\\d+\\)".toRegex()
-        val regexMatches = regex.findAll(lineToParse).also { println(it) }
-        val multiplyStrings = mutableListOf<String>()
+        val regexMatches = regex.findAll(lineToParse)
+        val dontMatchRegexMatches = dontMatchRegex.findAll(lineToParse)
+        val doMatchRegexMatches = doMatchRegex.findAll(lineToParse)
+
+        for (donotMatch in dontMatchRegexMatches) {
+            magicList.add(MagicList.DONT(donotMatch.range.last))
+        }
+
+        for (doMatch in doMatchRegexMatches) {
+            magicList.add(MagicList.DO(doMatch.range.last))
+        }
 
         for (match in regexMatches) {
-            multiplyStrings.add(match.value)
-            match.value.also { println(it) }
+            magicList.add(MagicList.MATCH(match.range.last, match.value))
         }
 
-        multiplyStrings.forEach { match ->
+        var ACTIVE = true
 
-            val firstNumber =
-                match.substringAfter("mul(").substringBefore(",").toInt().also { println(it) }
-            val secondNumber =
-                match.substringAfter(",").substringBefore(")").toInt().also { println(it) }
+        magicList.sortBy { it.startRange }
 
-            part1Total = part1Total.plus(firstNumber * secondNumber).also { println(it) }
-
+        for (item in magicList) {
+            when (item) {
+                is MagicList.DO -> {
+                    ACTIVE = true
+                }
+                is MagicList.DONT -> {
+                    ACTIVE = false
+                }
+                is MagicList.MATCH -> {
+                    if (ACTIVE) {
+                        multiplyStrings.add(item.calc).also { println(item.calc) }
+                    }
+                }
+            }
         }
-
-        "part1Total: $part1Total".println()
+        magicList.clear()
     }
+
+    multiplyStrings.forEach { match ->
+        val firstNumber =
+            match.substringAfter("mul(").substringBefore(",").toInt()
+        val secondNumber =
+            match.substringAfter(",").substringBefore(")").toInt()
+
+        part2Total = part2Total.plus(firstNumber * secondNumber)
+    }
+
+    "part2Total: $part2Total".println()
+
 }
